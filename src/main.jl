4097e94cc1e0aa603e7530a8978d1939d4a9df06
pvermees <p.vermeesch@ucl.ac.uk>
2024-08-08 09:32:16 +0100

    Moved Gtk application generator to initiation

--- src/main.jl
+++ src/main.jl
@@ -1,11 +1,14 @@
 function PTree!(tree::AbstractDict)
-
+    
     updateTree!(tree,
                 "dir|file";
                 action=Dict(
                     "d" => GUIloadICPdir!,
                     "p" => GUIloadICPfile!
                 ))
+
+    Gtk4.GLib.G_.set_application_name("Plasmatrace")
+    Gtk4.GLib.G_.set_prgname("Plasmatrace")
     
 end
 export PTree!
