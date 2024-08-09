87039fdfb52f1cdd63d953d1f0c8832239eedb8a
pvermees <p.vermeesch@ucl.ac.uk>
2024-08-09 14:42:34 +0100

    Added timestamp reader to GUI

--- src/GUIactions.jl
+++ src/GUIactions.jl
@@ -1,15 +1,18 @@
 function GUIloadICPdir!(ctrl::AbstractDict)
     dname = open_dialog("Choose a folder",ctrl["gui"];
                         select_folder=true,
-                        start_folder=ctrl["ICPpath"])
+                        start_folder=splitdir(ctrl["ICPpath"])[1])
     Plasmatrace.TUIloadICPdir!(ctrl,dname)
     return "xx"
 end
 export GUIloadICPdir!
 
 function GUIloadICPfile!(ctrl::AbstractDict)
-    fname = open_dialog("Choose a file",ctrl["gui"];
-                        start_folder=ctrl["ICPpath"])
-    return Plasmatrace.TUIloadICPfile!(ctrl,fname)
+    ctrl["ICPpath"] = open_dialog("Choose an ICP-MS data file",ctrl["gui"];
+                                  start_folder=splitdir(ctrl["ICPpath"])[1])
+    LApath = open_dialog("Choose a laser log file",ctrl["gui"];
+                         start_folder=splitdir(ctrl["ICPpath"])[1])
+    Plasmatrace.TUIloadLAfile!(ctrl,LApath)
+    return "xx"
 end
 export GUIloadICPfile!
