032de01ccf39612c33ff24012cd4e499be62313f
pvermees <p.vermeesch@ucl.ac.uk>
2024-08-09 15:12:54 +0100

    Asynchronous file readers

--- src/GUIactions.jl
+++ src/GUIactions.jl
@@ -1,18 +1,24 @@
 function GUIloadICPdir!(ctrl::AbstractDict)
-    dname = open_dialog("Choose a folder",ctrl["gui"];
-                        select_folder=true,
-                        start_folder=splitdir(ctrl["ICPpath"])[1])
-    Plasmatrace.TUIloadICPdir!(ctrl,dname)
+    open_dialog("Choose a folder",ctrl["gui"];
+                select_folder=true,
+                start_folder=splitdir(ctrl["ICPpath"])[1]) do dname
+                    @async Plasmatrace.TUIloadICPdir!(ctrl,dname)
+                end
+    ctrl["priority"]["load"] = false
     return "xx"
 end
 export GUIloadICPdir!
 
 function GUIloadICPfile!(ctrl::AbstractDict)
-    ctrl["ICPpath"] = open_dialog("Choose an ICP-MS data file",ctrl["gui"];
-                                  start_folder=splitdir(ctrl["ICPpath"])[1])
-    LApath = open_dialog("Choose a laser log file",ctrl["gui"];
-                         start_folder=splitdir(ctrl["ICPpath"])[1])
-    Plasmatrace.TUIloadLAfile!(ctrl,LApath)
+    open_dialog("Choose an ICP-MS data file",ctrl["gui"];
+                start_folder=splitdir(ctrl["ICPpath"])[1]) do ICPpath
+                    ctrl["ICPpath"] = ICPpath
+                    @async open_dialog("Choose a laser log file",ctrl["gui"];
+                                       start_folder=splitdir(ICPpath)[1]) do LApath
+                                           Plasmatrace.TUIloadLAfile!(ctrl,LApath)
+                                       end
+                end
+    ctrl["priority"]["load"] = false
     return "xx"
 end
 export GUIloadICPfile!
