2d1ab215048df48df60c67684724074373311348
Pieter <p.vermeesch@ucl.ac.uk>
2024-11-22 00:58:02 +0000

    Added toy Makie plot

--- src/PTgui.jl
+++ src/PTgui.jl
@@ -1,8 +1,10 @@
 module PTgui
 
-using Infiltrator, Gtk4
+using Infiltrator, Gtk4, Makie
 import Plasmatrace
 
 include("main.jl")
+include("dialogs.jl")
+include("plots.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,14 @@
-using PTgui, Test, Infiltrator
+using PTgui, Test, Infiltrator, GLMakie
 import Plasmatrace
 
 function ExtensionTest()
     Plasmatrace.PT(PTgui,logbook="test.log")
 end
 
-@testset "Extension test" begin ExtensionTest() end
+function MakieTest()
+    fig = MakiePlot()
+    display(fig)
+end
+
+#@testset "Extension test" begin ExtensionTest() end
+@testset "Makie test" begin MakieTest() end
