a102a7e853771a2af799e0d489616c2f80285f8f
Pieter <p.vermeesch@ucl.ac.uk>
2025-02-05 10:50:10 +0000

    Switching test to NHM data

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,26 @@
 using KJ, KJgui, Makie, Test, Infiltrator
 
 function MakieTest()
-    myrun = load("Lu-Hf",instrument="Agilent")
+    if true
+        myrun = load("/home/pvermees/Documents/Plasmatrace/NHM/240708_PV_Zircon.Iolite.csv",
+                     "/home/pvermees/Documents/Plasmatrace/NHM/240708_PV_Zircon_Maps.csv",
+                     instrument="Agilent")
+        method = "Lu-Hf";
+        channels = Dict("d"=>"Hf178 -> 260",
+                        "D"=>"Hf176 -> 258",
+                        "P"=>"Lu175 -> 175");
+        standards = Dict("Hogsbo_gt" => "hogsbo")
+        glass = Dict("NIST612" => "NIST612p")
+    else
+        myrun = load("Lu-Hf",instrument="Agilent")
+        method = "Lu-Hf";
+        channels = Dict("d"=>"Hf178 -> 260",
+                        "D"=>"Hf176 -> 258",
+                        "P"=>"Lu175 -> 175");
+        standards = Dict("Hogsbo_gt" => "hogsbo")
+        glass = Dict("NIST612" => "NIST612p")
+    end
     dt = dwelltime(myrun)
-    method = "Lu-Hf";
-    channels = Dict("d"=>"Hf178 -> 260",
-                    "D"=>"Hf176 -> 258",
-                    "P"=>"Lu175 -> 175");
-    standards = Dict("Hogsbo_gt" => "hogsbo")#"BP_gt" => "BP")#
-    glass = Dict("NIST612" => "NIST612p")
     blk, fit = process!(myrun,dt,method,channels,standards,glass,
                         nblank=2,ndrift=1,ndown=1);
     ctrl = Dict(
