724f0b777a61cbc81b8587e1e2483a0905349c7e
Pieter <p.vermeesch@ucl.ac.uk>
2025-02-06 11:19:52 +0000

    Changed default denominator settings in MakiePlot

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,31 +2,33 @@ using KJ, KJgui, Makie, Test, Infiltrator
 
 function MakieTest()
     if true
-        myrun = load("/home/pvermees/Documents/Plasmatrace/NHM/240708_PV_Zircon.Iolite.csv",
-                     "/home/pvermees/Documents/Plasmatrace/NHM/240708_PV_Zircon_Maps.csv",
+        myrun = load("/home/pvermees/Documents/Plasmatrace/NHM/240708_PV_Zircon_Maps.csv",
+                     "/home/pvermees/Documents/Plasmatrace/NHM/240708_PV_Zircon.Iolite.csv",
                      instrument="Agilent")
-        method = "Lu-Hf";
-        channels = Dict("d"=>"Hf178 -> 260",
-                        "D"=>"Hf176 -> 258",
-                        "P"=>"Lu175 -> 175");
-        standards = Dict("Hogsbo_gt" => "hogsbo")
-        glass = Dict("NIST612" => "NIST612p")
+        method = "U-Pb"
+        channels = Dict("d"=>"Pb207",
+                        "D"=>"Pb206",
+                        "P"=>"U238")
+        standards = Dict("91500_zr" => "91500")
+        glass = Dict("NIST612" => "NIST612")
+        den = "Pb206"
     else
         myrun = load("Lu-Hf",instrument="Agilent")
-        method = "Lu-Hf";
+        method = "Lu-Hf"
         channels = Dict("d"=>"Hf178 -> 260",
                         "D"=>"Hf176 -> 258",
-                        "P"=>"Lu175 -> 175");
+                        "P"=>"Lu175 -> 175")
         standards = Dict("Hogsbo_gt" => "hogsbo")
         glass = Dict("NIST612" => "NIST612p")
+        den = nothing
     end
     dt = dwelltime(myrun)
     blk, fit = process!(myrun,dt,method,channels,standards,glass,
-                        nblank=2,ndrift=1,ndown=1);
+                        nblank=2,ndrift=1,ndown=0)
     ctrl = Dict(
         "run" => myrun,
         "i" => 1,
-        "den" => nothing,
+        "den" => den,
         "method" => method,
         "channels" => channels,
         "standards" => standards,
@@ -35,10 +37,10 @@ function MakieTest()
         "blank" => blk,
         "dwell" => dt,
         "par" => fit,
-        "transformation" => "log"
+        "transformation" => "sqrt"
     )
     GUIinitPlotter!(ctrl)
-    MakiePlot!(ctrl,channels;transformation="log")
+    MakiePlot!(ctrl,channels;transformation="log",den=den)
 end
 
 function ExtensionTest()
